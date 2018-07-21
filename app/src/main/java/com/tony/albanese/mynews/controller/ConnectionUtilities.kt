9f96a0b3dc863f10f9f83f5bb49db838baac40ca
Tony Albanese <tony.albanese2000@gmail.com>
2018-07-21 19:36:57 +0200

    Added UnitTest to check connection validity.

--- app/src/main/java/com/tony/albanese/mynews/controller/ConnectionUtilities.kt
+++ app/src/main/java/com/tony/albanese/mynews/controller/ConnectionUtilities.kt
@@ -1,8 +1,10 @@
 package com.tony.albanese.mynews.controller
 
 import android.util.Log
+import java.io.IOException
 import java.net.MalformedURLException
 import java.net.URL
+import java.net.URLConnection
 
 fun stringToUrl(urlString: String): URL? {
     val url: URL
@@ -13,4 +15,15 @@ fun stringToUrl(urlString: String): URL? {
         return null
     }
     return url
+}
+
+fun connectToSite(url: URL): URLConnection?{
+    val testConnection: URLConnection
+    try{
+        testConnection = url.openConnection()
+    } catch (e: IOException){
+        Log.e("From: connectToURl", e.toString())
+        return null
+    }
+    return testConnection
 }
