e6995c53b2c82c772b13f18f6be9895f977812cf
Tony Albanese <tony.albanese2000@gmail.com>
2018-07-30 15:24:20 +0200

    Implemented pager adapter and added it to tab layout.

--- app/src/main/java/com/tony/albanese/mynews/controller/MainActivity.kt
+++ app/src/main/java/com/tony/albanese/mynews/controller/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.tony.albanese.mynews.controller
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import com.tony.albanese.mynews.R
+import com.tony.albanese.mynews.controller.adapters.TabPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    //Declare references for the pager adapter, the view pager, and the tab layout objects.
+    lateinit var pagerAdapter: TabPagerAdapter
+    lateinit var pager: ViewPager
+    lateinit var tabLayout: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        pagerAdapter = TabPagerAdapter(supportFragmentManager, applicationContext) //Initialize
+        pager = findViewById(R.id.view_pager_main_activity) //Initialize
+        tabLayout = findViewById(R.id.tab_layout_main_activity) //Initialize
+
+        pager.adapter = pagerAdapter //Set the adapter.
+        tabLayout.setupWithViewPager(pager) //Attach the pager to the tab layout.
+
     }
 }
