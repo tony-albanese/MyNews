8a6eef6dda5f1684abb488a42a8838f6defb642a
Tony Albanese <tony.albanese2000@gmail.com>
2018-07-30 12:40:13 +0200

    initial commit

---
+++ app/src/androidTest/java/com/tony/albanese/mynews/ServerResponseUnitTest.kt
@@ -0,0 +1,22 @@
+package com.tony.albanese.mynews
+
+import android.support.test.runner.AndroidJUnit4
+import com.tony.albanese.mynews.controller.utilities.connectToSite
+import com.tony.albanese.mynews.controller.utilities.readDataFromConnection
+import com.tony.albanese.mynews.controller.utilities.stringToUrl
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ServerResponseUnitTest {
+
+    @Test //This function tests the function that reads the response from the server with a valid REST api.
+    fun testReaderResponse() {
+        val url = stringToUrl("http://echo.jsontest.com/key/value/one/two")
+        val connection = connectToSite(url!!)
+        val response = readDataFromConnection(connection!!)
+        val expectedResponse = "{\"one\": \"two\",\"key\": \"value\"}"
+        Assert.assertEquals(expectedResponse.replace(" ", ""), response.replace(" ", ""))
+    }
+}
