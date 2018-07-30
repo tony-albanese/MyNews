08fb5b338060a1877d6054fbc5170df08c4bbda7
Tony Albanese <tony.albanese2000@gmail.com>
2018-07-30 12:56:01 +0200

    initial commit

---
+++ app/src/androidTest/java/com/tony/albanese/mynews/JsonControllerUnitTest.kt
@@ -0,0 +1,8 @@
+package com.tony.albanese.mynews
+
+//This class is for testing the JsonController.
+
+
+//TODO: Implement test for createSearchParametersJson()
+
+//TODO: Implement tests for parsing Json Object when the function is implemented in the controller.
