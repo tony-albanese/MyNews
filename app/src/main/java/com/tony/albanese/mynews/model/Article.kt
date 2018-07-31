b4eff1e46f277010c62d8d721b43da2d5f510888
Tony Albanese <tony.albanese2000@gmail.com>
2018-07-31 18:39:35 +0200

    Added new member variables for image url and snippet.

--- app/src/main/java/com/tony/albanese/mynews/model/Article.kt
+++ app/src/main/java/com/tony/albanese/mynews/model/Article.kt
@@ -1,9 +1,10 @@
 package com.tony.albanese.mynews.model
 
-class Article(headline:String, date: String, snippet: String, newsDesk: String, url:String) {
+class Article(headline:String, date: String, snippet: String, newsDesk: String, url:String, imageUrl: String) {
     val mHeadline = headline
     val mDate = date
     val mNewsDesk = newsDesk
-    val mImageUrl = url
+    val mUrl = url
+    val mImageUrl = imageUrl
     val mSnippet = snippet
 }
