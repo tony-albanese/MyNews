deb715db17dad81d97f2c66d6ad0d13f758a2cf4
Tony Albanese <tony.albanese2000@gmail.com>
2018-07-31 14:41:26 +0200

    Initial commit.

---
+++ app/src/main/java/com/tony/albanese/mynews/model/Article.kt
@@ -0,0 +1,8 @@
+package com.tony.albanese.mynews.model
+
+class Article(headline:String, date: String, newsDesk: String, url:String) {
+    val mHeadline = headline
+    val mDate = date
+    val mNewsDesk = newsDesk
+    val mImageUrl = url
+}
