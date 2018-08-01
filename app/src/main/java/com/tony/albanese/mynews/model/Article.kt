adbdf3796aed066e47fe786ce79df4e94e054e19
Tony Albanese <tony.albanese2000@gmail.com>
2018-08-01 10:54:28 +0200

    Initial commit.

---
+++ app/src/main/java/com/tony/albanese/mynews/model/Article.kt
@@ -0,0 +1,9 @@
+package com.tony.albanese.mynews.model
+
+class Article(title: String, section: String, publishedDate: String , url: String, imageUrl: String) {
+    var mTitle = title
+    var mSection = section
+    var mPublishedDate = publishedDate
+    var mUrl = url
+    var mImageUrl = imageUrl
+}
