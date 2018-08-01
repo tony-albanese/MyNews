cec0be51e3610bd615685e525eb7e7873ec81518
Tony Albanese <tony.albanese2000@gmail.com>
2018-08-01 15:11:11 +0200

    These models are for the MostPopular search results. They are generated from the Json2Pojo plugin.

---
+++ app/src/main/java/com/tony/albanese/mynews/model/MostPopularModel/MediaMetadatum.java
@@ -0,0 +1,51 @@
+
+package com.tony.albanese.mynews.model.MostPopularModel;
+
+import com.google.gson.annotations.Expose;
+
+
+@SuppressWarnings("unused")
+public class MediaMetadatum {
+
+    @Expose
+    private String format;
+    @Expose
+    private Long height;
+    @Expose
+    private String url;
+    @Expose
+    private Long width;
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public Long getHeight() {
+        return height;
+    }
+
+    public void setHeight(Long height) {
+        this.height = height;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Long getWidth() {
+        return width;
+    }
+
+    public void setWidth(Long width) {
+        this.width = width;
+    }
+
+}
---
+++ app/src/main/java/com/tony/albanese/mynews/model/MostPopularModel/MostPopular.java
@@ -0,0 +1,54 @@
+
+package com.tony.albanese.mynews.model.MostPopularModel;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+//@Generated("net.hexar.json2pojo")
+@SuppressWarnings("unused")
+public class MostPopular {
+
+    @Expose
+    private String copyright;
+    @SerializedName("num_results")
+    private Long numResults;
+    @Expose
+    private List<Result> results;
+    @Expose
+    private String status;
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
+
+    public Long getNumResults() {
+        return numResults;
+    }
+
+    public void setNumResults(Long numResults) {
+        this.numResults = numResults;
+    }
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
