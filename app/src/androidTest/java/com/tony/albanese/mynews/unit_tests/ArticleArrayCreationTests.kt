d081eb6367a497846daf1fbeee7eae3f5835c792
Tony Albanese <tony.albanese2000@gmail.com>
2018-08-01 11:38:48 +0200

    Deleted models. Starting over.

--- app/src/androidTest/java/com/tony/albanese/mynews/unit_tests/ArticleArrayCreationTests.kt
+++ app/src/androidTest/java/com/tony/albanese/mynews/unit_tests/ArticleArrayCreationTests.kt
@@ -2,8 +2,6 @@ package com.tony.albanese.mynews.unit_tests
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-import com.tony.albanese.mynews.controller.utilities.*
-import junit.framework.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -14,13 +12,7 @@ class ArticleArrayCreationTests {
     @Test
     fun testMostPopularArrayCreation(){
 
-        val url = generateSearchUrl(appContext,1)
-        //Make the connection and get the response.
-        val connection = connectToSite(stringToUrl(url)!!)
-        val response = readDataFromConnection(connection!!)
-        //Call generateArticleArray() to create the ArrayList
-        val list = generateArticleArray(1,response)
-        assertEquals(1, list.size)
+
     }
 
 }
