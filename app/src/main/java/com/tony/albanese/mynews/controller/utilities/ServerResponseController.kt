62e689953f1c984934c0bb20cf5f5072ef42191a
Tony Albanese <tony.albanese2000@gmail.com>
2018-08-01 15:59:30 +0200

    Made variables global in scope.

--- app/src/main/java/com/tony/albanese/mynews/controller/utilities/ServerResponseController.kt
+++ app/src/main/java/com/tony/albanese/mynews/controller/utilities/ServerResponseController.kt
@@ -3,29 +3,47 @@ package com.tony.albanese.mynews.controller.utilities
 import com.google.gson.Gson
 import com.tony.albanese.mynews.model.Article
 import com.tony.albanese.mynews.model.MostPopularModel.MostPopular
+import com.tony.albanese.mynews.model.TopStoriesModel.TopStories
 
 fun generateArticleArray(resultType: Int, response: String): ArrayList<Article>{
     var list = ArrayList<Article>()
-    val gson = Gson()
+    lateinit var title:String
+    lateinit var section: String
+    lateinit var date: String
+    lateinit var url: String
+    lateinit var imageUrl: String
 
     when(resultType){
         1 -> {
+            val gson = Gson()
             val mostPopular = gson.fromJson(response, MostPopular::class.java)
             val resultsArray = mostPopular.results
             for (i in 0..(resultsArray.size - 1)) {
                 var currentArticle = resultsArray[i]
-                val title = currentArticle.title
-                val section = currentArticle.section
-                val date = currentArticle.publishedDate
-                val url = currentArticle.url
-                val imageUrl = currentArticle.media[0].mediaMetadata[0].url
+                title = currentArticle.title
+                section = currentArticle.section
+                date = currentArticle.publishedDate
+                url = currentArticle.url
+                imageUrl = currentArticle.media[0].mediaMetadata[0].url
                 list.add(Article(title, section, date, url, imageUrl))
             }
             return list
         }
 
         2->{
+            val gson = Gson()
+            val topStories = gson.fromJson(response, TopStories::class.java)
+            val resultsArray = topStories.results
+            list.clear()
+            for(i in 0.. (resultsArray.size -1 )){
+                var currentArticle = resultsArray[i]
+                title = currentArticle.title
+                section = currentArticle.section
+                date = currentArticle.publishedDate
+                url = currentArticle.url
 
+                list.add(Article(title,section,date,url,"Dummy url"))
+            }
             return list
         }
         else -> return list
