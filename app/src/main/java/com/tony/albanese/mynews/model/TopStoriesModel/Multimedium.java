4009b522ab3eb146c6c1f3c205dbca1e47ba642f
Tony Albanese <tony.albanese2000@gmail.com>
2018-08-01 10:54:03 +0200

    Refactored package names

--- app/src/main/java/com/tony/albanese/mynews/model/TopStoriesModel/Multimedium.java
+++ app/src/main/java/com/tony/albanese/mynews/model/TopStoriesModel/Multimedium.java
@@ -1,5 +1,5 @@
 
-package com.tony.albanese.mynews;
+package com.tony.albanese.mynews.model.TopStoriesModel;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
