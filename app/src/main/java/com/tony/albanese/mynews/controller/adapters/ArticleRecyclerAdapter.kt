8bae58a4dafd9b575295faadb39fb75e5a688f31
Tony Albanese <tony.albanese2000@gmail.com>
2018-08-02 19:47:14 +0200

    Outlined functions to be overriden.

--- app/src/main/java/com/tony/albanese/mynews/controller/adapters/ArticleRecyclerAdapter.kt
+++ app/src/main/java/com/tony/albanese/mynews/controller/adapters/ArticleRecyclerAdapter.kt
@@ -1,4 +1,26 @@
 package com.tony.albanese.mynews.controller.adapters
 
-class ArticleRecyclerAdapter {
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class ArticleRecyclerAdapter: RecyclerView.Adapter<ArticleRecyclerAdapter.ArticleViewHolder>() {
+
+    class ArticleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ArticleViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: ArticleViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
 }
