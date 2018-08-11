bc7c4366b08cde6ec689201090a7d835c08cbd0d
Tony Albanese <tony.albanese2000@gmail.com>
2018-08-11 19:24:39 +0200

    Added functionality to get search terms.

--- app/src/main/java/com/tony/albanese/mynews/controller/activities/CustomSearchActivity.kt
+++ app/src/main/java/com/tony/albanese/mynews/controller/activities/CustomSearchActivity.kt
@@ -2,12 +2,31 @@ package com.tony.albanese.mynews.controller.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import com.tony.albanese.mynews.R
+import kotlinx.android.synthetic.main.search_screen_layout.*
 
 class CustomSearchActivity : AppCompatActivity() {
+    lateinit var searchButton: Button
+    lateinit var searchEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_screen_layout)
+
+        searchButton = btn_search
+        searchEditText = text_view_search_terms
+
+        btn_search.setOnClickListener { view ->
+            initiateSearch()
+        }
+    }
+
+    fun initiateSearch() {
+        var searchTerms = searchEditText.text.toString()
+        Log.i("Method: ", "initiateSearch() called.")
+        Log.i("Search terms:", searchTerms)
     }
 }
