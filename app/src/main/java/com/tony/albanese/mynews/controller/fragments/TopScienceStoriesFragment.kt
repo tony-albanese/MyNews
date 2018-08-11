60f02e8dfcd41abf4bb91604558388ae357e9bb6
Tony Albanese <tony.albanese2000@gmail.com>
2018-08-11 12:43:53 +0200

    Added implementation of doAsync() for network query.

--- app/src/main/java/com/tony/albanese/mynews/controller/fragments/TopScienceStoriesFragment.kt
+++ app/src/main/java/com/tony/albanese/mynews/controller/fragments/TopScienceStoriesFragment.kt
@@ -4,14 +4,17 @@ package com.tony.albanese.mynews.controller.fragments
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tony.albanese.mynews.R
 import com.tony.albanese.mynews.controller.adapters.ArticleRecyclerAdapter
-import com.tony.albanese.mynews.controller.utilities.generateSearchUrl
+import com.tony.albanese.mynews.controller.utilities.*
 import com.tony.albanese.mynews.model.Article
 import kotlinx.android.synthetic.main.fragment_base_layout.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 import java.util.*
 
 
@@ -24,6 +27,9 @@ import java.util.*
 class TopScienceStoriesFragment : Fragment() {
     var list = ArrayList<Article>()
     lateinit var scienceUrl: String
+    lateinit var articleAdapter: ArticleRecyclerAdapter
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_base_layout, container, false)
@@ -31,21 +37,34 @@ class TopScienceStoriesFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        val recyclerView = fragment_recycler_view
+        recyclerView = fragment_recycler_view
         val layoutManager = LinearLayoutManager(context)
-        val adapter = ArticleRecyclerAdapter(list)
         val dummyView = text_view_dummy
         scienceUrl = generateSearchUrl(context!!, 3)
 
+        fetchArticles()
 
         dummyView.text = "Top Science Stories"
         recyclerView.layoutManager = layoutManager
-        recyclerView.adapter = adapter
+
+
     }
 
     fun generateTestList(): ArrayList<Article> {
         list.add(Article("Title", "Section", "Date", "www.dd.dd", "www.dee.dd"))
         return list
+    }
+
+    fun fetchArticles() {
+        val connection = connectToSite(stringToUrl(scienceUrl)!!)
+        doAsync {
+            val result = readDataFromConnection(connection!!)
+            uiThread {
+                list = generateArticleArray(1, result)
+                articleAdapter = ArticleRecyclerAdapter(list)
+                recyclerView.adapter = articleAdapter
+            }
+        }
     }
 
 }
