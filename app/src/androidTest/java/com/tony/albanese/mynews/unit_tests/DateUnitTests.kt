6b0708911b06507f1997c6ab87d84ff373017440
Tony Albanese <tony.albanese2000@gmail.com>
2018-08-14 14:18:10 +0200

    Initial commit.

---
+++ app/src/androidTest/java/com/tony/albanese/mynews/unit_tests/DateUnitTests.kt
@@ -0,0 +1,13 @@
+package com.tony.albanese.mynews.unit_tests
+
+import android.support.test.runner.AndroidJUnit4
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+//TODO: Implement Date converter test.
+class DateUnitTests {
+    //Input Date String
+    //Convert it to Date Object
+    //Format it properly
+    //Conver to String
+}
