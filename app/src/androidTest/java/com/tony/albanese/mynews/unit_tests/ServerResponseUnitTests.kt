54da36d10d47f30ce3d48a70e41ad704501c8e47
Tony Albanese <tony.albanese2000@gmail.com>
2018-08-14 14:42:07 +0200

    Initial commit.

---
+++ app/src/androidTest/java/com/tony/albanese/mynews/unit_tests/ServerResponseUnitTests.kt
@@ -0,0 +1,8 @@
+package com.tony.albanese.mynews.unit_tests
+
+import android.support.test.runner.AndroidJUnit4
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ServerResponseUnitTests {
+}
