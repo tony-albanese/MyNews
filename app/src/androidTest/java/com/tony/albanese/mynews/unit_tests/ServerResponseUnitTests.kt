d5af462bab32509b9c26d21297893c73ea9c3edd
Tony Albanese <tony.albanese2000@gmail.com>
2018-08-18 07:52:37 +0200

    Outlined unit tests to be performed.

--- app/src/androidTest/java/com/tony/albanese/mynews/unit_tests/ServerResponseUnitTests.kt
+++ app/src/androidTest/java/com/tony/albanese/mynews/unit_tests/ServerResponseUnitTests.kt
@@ -5,4 +5,12 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ServerResponseUnitTests {
+    //TODO: Implement these tests.
+    /*
+    Need a test to process the server response for each of the search responses.
+
+    //Save a prepared response as a string.
+    //Convert it to an array.
+    //Verify the length of the array with an assert
+     */
 }
--- app/src/androidTest/java/com/tony/albanese/mynews/unit_tests/UITests.kt
+++ app/src/androidTest/java/com/tony/albanese/mynews/unit_tests/UITests.kt
@@ -5,4 +5,9 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class UITests {
+    //Test that if a user taps on a tab, the tab is dislpayed
+
+    //Test if a user types some search terms, these terms are contained in the box.
+
+    //Test if the user presses the search button, the search activity launched.
 }
