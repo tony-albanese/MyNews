407d654139a76912cb2eb096cefa820c9985f0d7
tony-albanese <tony.albanese2000@gmail.com>
2018-09-22 16:25:43 +0200

    Implemented onDateSet method and set values for date text views when user clicks.

--- app/src/main/java/com/tony/albanese/mynews/controller/activities/CustomSearchActivity.kt
+++ app/src/main/java/com/tony/albanese/mynews/controller/activities/CustomSearchActivity.kt
@@ -1,5 +1,6 @@
 package com.tony.albanese.mynews.controller.activities
 
+import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -10,27 +11,36 @@ import android.widget.DatePicker
 import android.widget.EditText
 import com.tony.albanese.mynews.R
 import com.tony.albanese.mynews.controller.fragments.DatePickerFragment
+import com.tony.albanese.mynews.controller.utilities.convertDate
 import com.tony.albanese.mynews.controller.utilities.createSearchParametersJson
 import com.tony.albanese.mynews.controller.utilities.generateSearchUrl
 import kotlinx.android.synthetic.main.search_screen_layout.*
 import java.util.*
 
 
-class CustomSearchActivity : AppCompatActivity() {
+class CustomSearchActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
     lateinit var searchButton: Button
     lateinit var searchEditText: EditText
     lateinit var datePicker: DatePicker
     lateinit var url: String
+    //lateinit var searchStartDate: String
+    //lateinit var searchEndDate: String
+    lateinit var selectedDate: String
+    lateinit var calendar: Calendar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_screen_layout)
+
         searchButton = btn_search
         searchEditText = text_view_search_terms
 
-        var calendar = Calendar.getInstance()
-        var year = calendar.get(Calendar.YEAR)
-        var month = calendar.get(Calendar.MONTH)
-        var dat = calendar.get(Calendar.DAY_OF_MONTH)
+        calendar = Calendar.getInstance()
+        selectedDate = convertDate(calendar.time)
+
+
+
+
 
         btn_search.setOnClickListener { view ->
             initiateSearch()
@@ -58,6 +68,24 @@ class CustomSearchActivity : AppCompatActivity() {
     fun createDatePicker(view: View) {
         val newFragment = DatePickerFragment()
         newFragment.show(supportFragmentManager, "datePicker")
+
+        when (view.id) {
+            R.id.tv_start_date -> {
+                tv_start_date.text = selectedDate
+
+            }
+
+            R.id.tv_end_date -> {
+                tv_end_date.text = selectedDate
+
+            }
+        }
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        calendar.set(year, month, dayOfMonth)
+        val date: Date = calendar.time
+        selectedDate = convertDate(date)
     }
 
 }
