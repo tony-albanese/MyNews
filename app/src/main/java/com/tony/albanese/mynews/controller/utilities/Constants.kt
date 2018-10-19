5a3842b59878f788517fbf8a993c8c9108872c2e
Tony Albanese <tony.albanese2000@gmail.com>
2018-10-19 19:37:09 +0200

    Added constant for most popular search.

--- app/src/main/java/com/tony/albanese/mynews/controller/utilities/Constants.kt
+++ app/src/main/java/com/tony/albanese/mynews/controller/utilities/Constants.kt
@@ -2,6 +2,7 @@ package com.tony.albanese.mynews.controller.utilities
 
 val ARTICLE_PREFERENCES = "article_preferences"
 val TOP_STORIES = "top_stories"
+val MOST_POPULAR = "most_popular"
 
 val MOST_POPULAR_SEARCH = 1
 val TOP_STORIES_SEARCH = 2
