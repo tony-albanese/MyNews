2b3adf6580f7e445f3e94220fa248585516d127c
Tony Albanese <tony.albanese2000@gmail.com>
2018-10-20 10:55:10 +0200

    Added constants for the science article search.

--- app/src/main/java/com/tony/albanese/mynews/controller/utilities/Constants.kt
+++ app/src/main/java/com/tony/albanese/mynews/controller/utilities/Constants.kt
@@ -3,6 +3,9 @@ package com.tony.albanese.mynews.controller.utilities
 val ARTICLE_PREFERENCES = "article_preferences"
 val TOP_STORIES = "top_stories"
 val MOST_POPULAR = "most_popular"
+val TOP_SCIENCE = "top_science"
 
 val MOST_POPULAR_SEARCH = 1
 val TOP_STORIES_SEARCH = 2
+val TOP_SCIENCE_SEARCH = 3
+val TOP_SCIENCE_RESULTS = 1
