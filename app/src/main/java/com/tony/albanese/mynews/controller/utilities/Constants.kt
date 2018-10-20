f6e427dcd0b39a315252bbfbb0f503d81ad8fdd7
Tony Albanese <tony.albanese2000@gmail.com>
2018-10-20 19:04:05 +0200

    Added more constants for SharedPreferences and custom searches.

--- app/src/main/java/com/tony/albanese/mynews/controller/utilities/Constants.kt
+++ app/src/main/java/com/tony/albanese/mynews/controller/utilities/Constants.kt
@@ -1,6 +1,10 @@
 package com.tony.albanese.mynews.controller.utilities
 
+val URL = "URL"
+val TAB = "TAB"
+
 val ARTICLE_PREFERENCES = "article_preferences"
+val URL_SHARED_PREFERENCES = "url_preferences"
 val TOP_STORIES = "top_stories"
 val MOST_POPULAR = "most_popular"
 val TOP_SCIENCE = "top_science"
@@ -8,4 +12,10 @@ val TOP_SCIENCE = "top_science"
 val MOST_POPULAR_SEARCH = 1
 val TOP_STORIES_SEARCH = 2
 val TOP_SCIENCE_SEARCH = 3
+val CUSTOM_SEARCH_SEARCH = 4
+
 val TOP_SCIENCE_RESULTS = 1
+
+val CUSTOM_SEARCH_TAB = 3
+
+val SEARCH_DATE_FORMAT = "yyyMMdd"
