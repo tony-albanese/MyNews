3c84ee65d3d02f6b431d05e170b5cb1d29518de2
Tony Albanese <tony.albanese2000@gmail.com>
2018-10-21 13:26:24 +0200

    Added network check before URL is loaded. Set visibility property of WebView and TextView in case of network error.

--- app/src/main/java/com/tony/albanese/mynews/controller/activities/WebViewActivity.kt
+++ app/src/main/java/com/tony/albanese/mynews/controller/activities/WebViewActivity.kt
@@ -2,6 +2,9 @@ package com.tony.albanese.mynews.controller.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.webkit.WebView
+import android.widget.TextView
 import android.widget.Toast
 import com.tony.albanese.mynews.R
 import com.tony.albanese.mynews.controller.utilities.URL_EXTRA
@@ -13,16 +16,22 @@ class WebViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
+        val webView = findViewById<WebView>(R.id.article_web_view)
+        val emptyView = findViewById<TextView>(R.id.tv_empty_view)
 
         val intent = getIntent()
-        if (intent != null) {
+        if (intent != null && networkIsAvailable(this)) {
             val url = intent.extras.getString(URL_EXTRA)
-            if (!url.isNullOrEmpty() && !url.isNullOrBlank() && networkIsAvailable(this)) {
+            if (!url.isNullOrEmpty() && !url.isNullOrBlank()) {
                 article_web_view.loadUrl(url)
+                webView.visibility = View.VISIBLE
+                emptyView.visibility = View.GONE
             }
         } else {
             val toast = Toast.makeText(this, "An error occured", Toast.LENGTH_SHORT)
             toast.show()
+            webView.visibility = View.GONE
+            emptyView.visibility = View.VISIBLE
         }
 
     }
