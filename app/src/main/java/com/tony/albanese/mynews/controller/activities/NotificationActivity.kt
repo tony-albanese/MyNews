6fee880e4a2963797a1227904aa97fa74cdb5724
Tony Albanese <tony.albanese2000@gmail.com>
2018-10-29 20:11:31 +0100

    Initial commit.

---
+++ app/src/main/java/com/tony/albanese/mynews/controller/activities/NotificationActivity.kt
@@ -0,0 +1,13 @@
+package com.tony.albanese.mynews.controller.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.tony.albanese.mynews.R
+
+class NotificationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.search_parameters_layout)
+    }
+}
