c7840f45ca027d4d4ecea2581a7b0cf3e44d32f1
Tony Albanese <tony.albanese2000@gmail.com>
2018-10-31 18:04:50 +0100

    Started to define the notification channel.

--- app/src/main/java/com/tony/albanese/mynews/model/SearchAlarmReceiver.kt
+++ app/src/main/java/com/tony/albanese/mynews/model/SearchAlarmReceiver.kt
@@ -1,27 +1,31 @@
 package com.tony.albanese.mynews.model
 
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import com.tony.albanese.mynews.R
 
 class SearchAlarmReceiver : BroadcastReceiver() {
+    private var notificationManager: NotificationManager? = null
 
     val channelName = "MyNews"
+    val channelDescription = "Description from NewsApp"
     val channelId = "NEWS_CHANNEL"
     override fun onReceive(context: Context?, intent: Intent?) {
+
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
-            channel.enableLights(true)
-            channel.enableVibration(true)
-            channel.lightColor = R.color.colorPrimary
-            channel.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, channelName, importance).apply {
+                description = channelDescription
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
 
-
     }
+
 }
