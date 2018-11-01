8c216d79fd00412e7a71ace649ef1ce01b247340
Tony Albanese <tony.albanese2000@gmail.com>
2018-11-01 21:33:14 +0100

    Implemented code that will return a DatePicker dialog fragment.

--- app/src/main/java/com/tony/albanese/mynews/controller/fragments/TimePickerFragment.kt
+++ app/src/main/java/com/tony/albanese/mynews/controller/fragments/TimePickerFragment.kt
@@ -1,4 +1,19 @@
 package com.tony.albanese.mynews.controller.fragments
 
-class TimePickerFragment {
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import java.util.*
+
+class TimePickerFragment : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val c = Calendar.getInstance()
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+        val is24HourFormat = android.text.format.DateFormat.is24HourFormat(activity)
+        return TimePickerDialog(context, activity as TimePickerDialog.OnTimeSetListener, hour, minute, is24HourFormat)
+        //return super.onCreateDialog(savedInstanceState)
+    }
 }
