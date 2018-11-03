e19e5119f971746b13e5ffe35b2da5dd5932b215
Tony Albanese <tony.albanese2000@gmail.com>
2018-11-03 11:35:50 +0100

    Starting to build out the deliverNotification() method.

--- app/src/main/java/com/tony/albanese/mynews/model/SearchAlarmReceiver.kt
+++ app/src/main/java/com/tony/albanese/mynews/model/SearchAlarmReceiver.kt
@@ -1,15 +1,26 @@
 package com.tony.albanese.mynews.model
 
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.support.v4.app.NotificationCompat
+import com.tony.albanese.mynews.controller.activities.MainActivity
+import com.tony.albanese.mynews.controller.utilities.NOTIFICATION_CHANNEL_ID
+import com.tony.albanese.mynews.controller.utilities.SEARCH_ALARM_CODE
 
 class SearchAlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        
+        sendNotification(context!!)
 
     }
 
+    fun sendNotification(context: Context) {
+        val notificationIntent = Intent(context, MainActivity::class.java)
+        val notificationPendingIntent = PendingIntent.getActivity(context, SEARCH_ALARM_CODE, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val builder = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+        
 
+    }
 }
