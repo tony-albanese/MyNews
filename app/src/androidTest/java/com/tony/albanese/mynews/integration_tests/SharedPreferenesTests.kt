6a59b1357081d8dbfb8d9de91719e95190862f64
Tony Albanese <tony.albanese2000@gmail.com>
2018-11-08 06:21:38 +0100

    Added code to generate the ArrayLists for testing.

--- app/src/androidTest/java/com/tony/albanese/mynews/integration_tests/SharedPreferenesTests.kt
+++ app/src/androidTest/java/com/tony/albanese/mynews/integration_tests/SharedPreferenesTests.kt
@@ -5,8 +5,10 @@ import android.content.SharedPreferences
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import com.google.gson.Gson
-import com.tony.albanese.mynews.controller.utilities.ARTICLE_PREFERENCES
+import com.tony.albanese.mynews.controller.utilities.*
 import com.tony.albanese.mynews.model.Article
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 import org.junit.Before
 import org.junit.runner.RunWith
 import java.net.HttpURLConnection
@@ -20,16 +22,35 @@ class SharedPreferenesTests {
     var list2 = ArrayList<Article>()
 
     val gson = Gson()
-    val url1 = ""
-    val url2 = ""
+    var url1 = ""
+    var url2 = ""
 
     @Before
     fun setup() {
         val context = InstrumentationRegistry.getContext()
         preferences = context.getSharedPreferences(ARTICLE_PREFERENCES, Context.MODE_PRIVATE)
 
-        val connection = HttpURLConnection()
+        url1 = generateSearchUrl(context, MOST_POPULAR_SEARCH)
+        url2 = generateSearchUrl(context, TOP_SCIENCE_SEARCH)
+        val connection = connectToSite(stringToUrl(url1)!!)
+        val connection2 = connectToSite(stringToUrl(url2)!!)
+
+        list1 = getArticles(connection!!, MOST_POPULAR_SEARCH)
+        list2 = getArticles(connection2!!, TOP_SCIENCE_RESULTS)
 
     }
 
+    fun getArticles(connection: HttpURLConnection, resultType: Int): ArrayList<Article> {
+        var result = ""
+        var list = ArrayList<Article>()
+        doAsync {
+            result = readDataFromConnection(connection)
+            uiThread {
+                list = generateArticleArray(resultType, result)
+            }
+        }
+        return list
+    }
+
+    
 }
