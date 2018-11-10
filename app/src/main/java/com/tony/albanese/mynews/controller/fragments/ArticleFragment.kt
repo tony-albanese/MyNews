cdde433b8fe8e527a4ff9e3be1ae6b1723d13556
Tony Albanese <tony.albanese2000@gmail.com>
2018-11-10 22:06:10 +0100

    Implemented onCreateView()

--- app/src/main/java/com/tony/albanese/mynews/controller/fragments/ArticleFragment.kt
+++ app/src/main/java/com/tony/albanese/mynews/controller/fragments/ArticleFragment.kt
@@ -1,4 +1,26 @@
 package com.tony.albanese.mynews.controller.fragments
 
-class ArticleFragment {
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tony.albanese.mynews.R
+
+
+/*
+This class will serve as the basis for the other article fragments in order to reduce the repeated code.
+ */
+
+class ArticleFragment : Fragment() {
+    lateinit var swipeLayout: SwipeRefreshLayout
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_base_layout, container, false)
+        swipeLayout = view.findViewById(R.id.swipe_refresh_layout)
+        return view
+    }
+
+    
 }
