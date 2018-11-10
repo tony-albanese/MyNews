f3dfd73d2a611d83bc2e42de1a1f316f8265ba8d
Tony Albanese <tony.albanese2000@gmail.com>
2018-11-10 22:17:00 +0100

    Added function for the click listener and initialized recycler view and ArrayList<Article>

--- app/src/main/java/com/tony/albanese/mynews/controller/fragments/ArticleFragment.kt
+++ app/src/main/java/com/tony/albanese/mynews/controller/fragments/ArticleFragment.kt
@@ -1,12 +1,23 @@
 package com.tony.albanese.mynews.controller.fragments
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tony.albanese.mynews.R
+import com.tony.albanese.mynews.controller.activities.WebViewActivity
+import com.tony.albanese.mynews.controller.adapters.ArticleRecyclerAdapter
+import com.tony.albanese.mynews.controller.utilities.ARTICLE_PREFERENCES
+import com.tony.albanese.mynews.controller.utilities.URL_EXTRA_KEY
+import com.tony.albanese.mynews.model.Article
+import kotlinx.android.synthetic.main.fragment_base_layout.*
+import java.util.*
 
 
 /*
@@ -15,6 +26,12 @@ This class will serve as the basis for the other article fragments in order to r
 
 class ArticleFragment : Fragment() {
     lateinit var swipeLayout: SwipeRefreshLayout
+    lateinit var recyclerView: RecyclerView
+    lateinit var preferences: SharedPreferences
+    lateinit var articleAdapter: ArticleRecyclerAdapter
+
+    var list = ArrayList<Article>()
+    var tempList = ArrayList<Article>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_base_layout, container, false)
@@ -22,5 +39,27 @@ class ArticleFragment : Fragment() {
         return view
     }
 
-    
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val layoutManager = LinearLayoutManager(context)
+        val subjectView = text_view_subject
+        recyclerView = fragment_recycler_view
+        preferences = activity!!.getSharedPreferences(ARTICLE_PREFERENCES, 0)
+        articleAdapter = ArticleRecyclerAdapter(list, context!!, { view: View, article: Article -> onArticleClicked(view, article) })
+        recyclerView.adapter = articleAdapter
+        recyclerView.layoutManager = layoutManager
+
+        // topStoriesUrl = generateSearchUrl(context!!, TOP_STORIES_SEARCH)
+        // subjectView.text = "Top Stories"
+    }
+
+    //This function is calle when the user clicks an article.
+    fun onArticleClicked(view: View, article: Article) {
+        view.setBackgroundColor(resources.getColor(R.color.colorIsRead))
+        article.mIsRead = true
+        val intent = Intent(context, WebViewActivity::class.java).apply {
+            putExtra(URL_EXTRA_KEY, article.mUrl)
+        }
+        startActivity(intent)
+    }
 }
