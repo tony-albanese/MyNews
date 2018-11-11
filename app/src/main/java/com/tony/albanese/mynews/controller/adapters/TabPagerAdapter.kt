673b547c445b6a6d523e54e2b57e9cd50d6da9be
Tony Albanese <tony.albanese2000@gmail.com>
2018-11-11 11:32:54 +0100

    Switched call from individual fragments to ArticleFragmen().getInstance()

--- app/src/main/java/com/tony/albanese/mynews/controller/adapters/TabPagerAdapter.kt
+++ app/src/main/java/com/tony/albanese/mynews/controller/adapters/TabPagerAdapter.kt
@@ -6,10 +6,10 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.PagerAdapter
 import com.tony.albanese.mynews.R
-import com.tony.albanese.mynews.controller.fragments.*
-import com.tony.albanese.mynews.controller.utilities.TOP_STORIES_KEY
-import com.tony.albanese.mynews.controller.utilities.TOP_STORIES_RESULTS
-import com.tony.albanese.mynews.controller.utilities.TOP_STORIES_SEARCH
+import com.tony.albanese.mynews.controller.fragments.ArticleFragment
+import com.tony.albanese.mynews.controller.fragments.CustomSearchFragment
+import com.tony.albanese.mynews.controller.fragments.TopStoriesFragment
+import com.tony.albanese.mynews.controller.utilities.*
 
 class TabPagerAdapter(fragmentManager: FragmentManager, context: Context) : FragmentPagerAdapter(fragmentManager) {
 
@@ -21,9 +21,8 @@ class TabPagerAdapter(fragmentManager: FragmentManager, context: Context) : Frag
     override fun getItem(postion: Int): Fragment {
         when (postion) {
             0 -> return ArticleFragment().newInstance(TOP_STORIES_SEARCH, TOP_STORIES_RESULTS, TOP_STORIES_KEY)
-            // 0 -> return TopStoriesFragment()
-            1 -> return MostPopularFragment()
-            2 -> return TopScienceStoriesFragment()
+            1 -> return ArticleFragment().newInstance(MOST_POPULAR_SEARCH, MOST_POPULAR_RESULTS, MOST_POPULAR_KEY)
+            2 -> return ArticleFragment().newInstance(TOP_SCIENCE_SEARCH, TOP_SCIENCE_RESULTS, TOP_SCIENCE_KEY)
             3 -> return CustomSearchFragment()
             else -> return TopStoriesFragment()
         }
@@ -35,7 +34,7 @@ class TabPagerAdapter(fragmentManager: FragmentManager, context: Context) : Frag
             1 -> return mContext.getString(R.string.most_read_tab_title)
             2 -> return mContext.getString(R.string.science_tab_title)
             3 -> return mContext.getString(R.string.custom_search_title)
-            else -> return "No title found."
+            else -> return "Articles"
 
         }
     }
