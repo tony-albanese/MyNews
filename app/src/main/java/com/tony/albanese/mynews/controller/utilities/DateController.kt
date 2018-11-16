730ea196176b5bd57659a1b2280c7daede3615af
tony-albanese <tony.albanese2000@gmail.com>
2018-11-16 19:02:19 +0100

    Changed error response of convertDate() function and added comments.

--- app/src/main/java/com/tony/albanese/mynews/controller/utilities/DateController.kt
+++ app/src/main/java/com/tony/albanese/mynews/controller/utilities/DateController.kt
@@ -4,9 +4,8 @@ import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
-/*
-formatArticleDate() -- Formats the date to be displayed in the app.
-convertDate() -- This accepts a date object and converts the date to string for the search.
+/**
+ * This function accepts a string and formats it for display in an Article object.
  */
 fun formatArticleDate(date: String): String {
     val initialFormat = SimpleDateFormat("yyy-MM-dd")
@@ -18,11 +17,14 @@ fun formatArticleDate(date: String): String {
         return formattedDate
     } catch (e: ParseException) {
         e.printStackTrace()
-        return "No date"
+        return ERROR
     }
 
 }
 
+/**
+ * Accepts a Date object and a format string and converts it into a format required by the NYT API.
+ */
 fun convertDate(date: Date, formatString: String = "dd/MM/yyy"): String {
     val dateFormat = SimpleDateFormat(formatString) //Date should be in 02/02/1982 format.
     val dateString = dateFormat.format(date)
