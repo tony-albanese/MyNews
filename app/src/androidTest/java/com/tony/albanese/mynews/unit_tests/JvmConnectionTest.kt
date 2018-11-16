0a4cadaceb6d0cb4022de1f018305624a5c26536
tony-albanese <tony.albanese2000@gmail.com>
2018-11-16 16:21:49 +0100

    Made this instrumentation test.

--- app/src/androidTest/java/com/tony/albanese/mynews/unit_tests/JvmConnectionTest.kt
+++ app/src/androidTest/java/com/tony/albanese/mynews/unit_tests/JvmConnectionTest.kt
@@ -1,15 +1,18 @@
-package com.tony.albanese.mynews
+package com.tony.albanese.mynews.unit_tests
 
+import android.support.test.runner.AndroidJUnit4
 import com.tony.albanese.mynews.controller.utilities.stringToUrl
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
 import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(AndroidJUnit4::class)
 class JvmConnectionTest {
     @Test
     fun addition_isCorrect() {
